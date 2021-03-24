da9948b87b24ee940a34de37939aba5141fbd44d
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-03-24 15:50:56 -0400

    component: correct last commit with saved grid file

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -12,47 +12,6 @@ import djikstra from "../helpers/djikstraHelpers";
 
 import "../Styles/Grid.css";
 
-// const START_NODE_ROW = 7;
-// const START_NODE_COL = 5;
-
-// const FINISH_NODE_ROW = 7;
-// const FINISH_NODE_COL = 45;
-
-// const createNode = (row, col) => {
-//   const node = {
-//     row,
-//     col,
-//     isStart: row === START_NODE_ROW && col === START_NODE_COL,
-//     isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
-//     distance: Infinity,
-//     isVisited: false,
-//     isWall: false,
-//     previousNode: null,
-//   };
-
-//   return node;
-// };
-
-// create the initial array of node objects
-// const iniGrid = () => {
-//   const grid = [];
-
-//   // for each row in the grid...
-//   for (let row = 0; row < 15; row++) {
-//     const currentRow = [];
-
-//     // for each column in the row...
-//     for (let col = 0; col < 50; col++) {
-//       // create node and push
-//       currentRow.push(createNode(row, col));
-//     }
-
-//     grid.push(currentRow);
-//   }
-
-//   return grid;
-// };
-
 export default function Grid() {
   // const [nodes, setNodes] = useState([])
 
