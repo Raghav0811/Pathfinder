8bf56874cddfb7a9881dcbe648a97819622f8aa3
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-03-26 12:00:32 -0400

    styling: wall styling to node

--- src/Pathfinder/Node/Node.jsx
+++ src/Pathfinder/Node/Node.jsx
@@ -1,11 +1,12 @@
 const classNames = require("classnames");
 
 export default function Node(props) {
-  const { row, col, isStart, isFinish, isVisited } = props;
+  const { row, col, isStart, isFinish, isVisited, isWall } = props;
 
   const classes = classNames("Node", {
     "node-start": isStart,
     "node-finish": isFinish,
+    "node-wall": isWall,
   });
 
   return <div id={`node-${row}-${col}`} className={classes}></div>;
